1abd3cc26037d214652ccf551010e94892bcefbe
Nikan <njavadzadeh@gmail.com>
2022-10-11 14:39:39 -0700

    commit tree e7dcb460125d34cb9c7322a56e2378b0c009f596

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee={
+name:"Nikan",
+streetAddress:"6923 e. magdalena dr."
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value)
+{
+    const newObj= {...employee}
+    newObj[key]=value;
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value)
+{
+    const newObj=employee;
+    newObj[key]=value;
+    return newObj;
+}
+function deleteFromEmployeeByKey(employee,key)
+{
+    const newObj={...employee};
+    delete newObj[key];
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key)
+{
+    delete employee[key]
+    return employee;
+}
